8c8170d912acad4615da47aaf71caff41d0f59fa
BindushreeGD <bindugd2001@gmail.com>
2026-01-27 18:08:26 +0530

    Initial admin panel push

--- main.py
+++ main.py
@@ -23,7 +23,13 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"^http://(localhost|127\.0\.0\.1)(:\d+)?$",
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://voidworksgroup.co.uk",
+        "https://www.voidworksgroup.co.uk"
+    ],
+    allow_origin_regex=r"^http://(localhost|127.0.0.1)(:\d+)?$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
